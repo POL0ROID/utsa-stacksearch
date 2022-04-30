1576aaaf157d2d1e57586f4c775ea2ace4afa105
EC2 Default User <ec2-user@ip-10-0-12-195.ec2.internal>
2022-04-30 23:08:17 +0000

    commit

--- server/stackserve.js
+++ server/stackserve.js
@@ -1,8 +1,18 @@
 const Koa = require('koa');
+const Router = require('@koa/router');
 const app = new Koa();
+const router = new Router();
 
-app.use(async ctx => {
-    ctx.body = 'Hello World';
-});
+// app.use(async ctx => {
+    // ctx.body = 'Hello World';
+// });
 
+
+router.get("/master", (ctx, next) => {
+	console.log("SERVER GOT REQUEST!");
+	ctx.body = "hello paul!!!";
+})
+
+app.use( router.routes() );
+console.log("SERVER is RUNNING!");
 app.listen(3001);
