fdc5d06948c1f9245f0bd8e088672d7ab5a0ba80
EC2 Default User <ec2-user@ip-10-0-12-195.ec2.internal>
2022-05-01 03:54:43 +0000

    commit

--- server/stackserve.js
+++ server/stackserve.js
@@ -1,18 +1,31 @@
 const Koa = require('koa');
 const Router = require('@koa/router');
+const parser = require('koa-bodyparser')
 const app = new Koa();
 const router = new Router();
 
-// app.use(async ctx => {
-    // ctx.body = 'Hello World';
-// });
 
 
 router.get("/query", (ctx, next) => {
-	console.log("SERVER GOT REQUEST!");
+	console.log("SERVER GOT GET REQUEST!");
 	ctx.body = "hello paul!!!";
 })
 
+router.post("/query", (ctx, next) => {
+	console.log("SERVER GOT POST REQUEST!");
+	console.log( "POST BODY", ctx.request.body );
+	console.log( "POST callMe: ", ctx.request.body.callMe );
+	// console.log("CTX", ctx);
+	// console.log("NEXT", next);
+	ctx.body = "post hello paul!!!";
+	// next( ctx );
+})
+
+//app.use(async ctx => {
+//	ctx.body = ctx.request.body;
+//});
+
+app.use( parser() );
 app.use( router.routes() );
 console.log("SERVER is RUNNING!");
 app.listen(3001);
