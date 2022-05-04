c6d4b2b4fb9e513d1f52fea12a420a73c4f725cf
POL0ROID <donflamon@gmail.com>
2022-05-04 08:18:32 -0500

    commit

--- server/stackserve.js
+++ server/stackserve.js
@@ -35,7 +35,7 @@ router.post("/stackserve.js", (ctx, next) => {
 	const viewquery = "SELECT ViewCount, COUNT(*) FROM #MyQuery GROUP BY ViewCount;";
 	const scorequery = "SELECT Score, COUNT(*) FROM #MyQuery GROUP BY Score;";
 	const datequery = "SELECT year, month, COUNT(*) FROM #MyQuery GROUP BY year, month;";
-	test = client.query("SELECT NOW();", (err, res) => {
+	test = client.query("SELECT NOW() as now;", (err, res) => {
 		console.log(err, res);
 		console.log("And THAT'S Games! :cooldude:");
 	})
