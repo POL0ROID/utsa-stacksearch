4722330d3844c2b1ef033a67ac0b6e3509368369
POL0ROID <donflamon@gmail.com>
2022-05-04 13:44:22 -0500

    commit

--- server/stackserve.js
+++ server/stackserve.js
@@ -57,7 +57,7 @@ router.post("/stackserve.js", async (ctx, next) => {
 	var res7 = `${JSON.stringify(r3)}${JSON.stringify(r4)}${JSON.stringify(r5)}${JSON.stringify(r6)}`.replace("}{", "}!@#$%^&*(){")
 	var res8 = res7.split("!@#$%^&*()");
 	console.log("r3: " + res8[0]);
-	console.log(JSON.parse(r8[0]))
+	console.log(JSON.parse(res8[0]));
 //	res7 = "{ " + JSON.stringify(result2) + " " + JSON.stringify(result3) + " " + JSON.stringify(result4) + " " + JSON.stringify(result5) + " " + JSON.stringify(result6) + " }";
 	console.log(JSON.parse(res8));
 	//res = JSON.parse(res7);
