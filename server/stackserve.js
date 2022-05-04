62c21ad06b58bb04f9240cae9bf2633ae39ff817
POL0ROID <donflamon@gmail.com>
2022-05-04 07:10:17 -0500

    commit

--- server/stackserve.js
+++ server/stackserve.js
@@ -171,5 +171,5 @@ let httpssl = https.createServer(
 app.use( cors() );
 app.use( router.routes() );
 console.log("Server is listening.");
-app.listen(3000);
+app.listen(3001);
 //httpssl.listen(443, err => {if (err) console.log(err); });
