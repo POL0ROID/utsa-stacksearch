56eceb593ddf4f03e44329a993d366d59c65d65c
POL0ROID <donflamon@gmail.com>
2022-05-04 12:17:23 -0500

    commit

--- server/stackserve.js
+++ server/stackserve.js
@@ -37,31 +37,21 @@ router.post("/stackserve.js", async (ctx, next) => {
 	console.log(jstring);
 	const json = JSON.parse(jstring);
 	console.log(json);
-	const basequery = queryconstruct(json);
-	const qandaquery = "CREATE TEMP TABLE MyQuery2 AS SELECT PostTypeId, ParentOrChild, COUNT(*) FROM MyQuery GROUP BY PostTypeId, ParentOrChild;";
+	const basetable= queryconstruct(json);
+	const qandatable = "CREATE TEMP TABLE MyQuery2 AS SELECT PostTypeId, ParentOrChild, COUNT(*) FROM MyQuery GROUP BY PostTypeId, ParentOrChild;";
+	const qandaquery = `SELECT * FROM MyQuery2;`
 	const totalquery = "SELECT PostTypeId, COUNT(*) FROM MyQuery2 GROUP BY PostTypeId;";
 	const viewquery = "SELECT ViewCount, COUNT(*) FROM MyQuery GROUP BY ViewCount;";
 	const scorequery = "SELECT Score, COUNT(*) FROM MyQuery GROUP BY Score;";
 	const datequery = "SELECT year, month, COUNT(*) FROM MyQuery GROUP BY year, month;";
-	result1 = client.query(basequery, async (err, res) => {
-		console.log(err, res);
-	});
-	result2 = client.query(qandaquery, async (err, res) => {
-		console.log(err, res);
-	});
-	result3 = client.query(totalquery, async (err, res) => {
-		console.log(err, res);
-	});
-	result4 = client.query(viewquery, async (err, res) => {
-		console.log(err, res);
-	});
-	result5 = client.query(scorequery, async (err, res) => {
-		console.log(err, res);
-	});
-	result6 = client.query(datequery, async (err, res) => {
-		console.log(err, res);
-	});
-	res7 = "{ " + await result2 + " " + await result3 + " " + await result4 + " " + await result5 + " " + await result6 + " }";
+	result1 = client.query(basetable);
+	result2 = client.query(qandatable);
+	result2 = client.query(qandaquery);
+	result3 = client.query(totalquery);
+	result4 = client.query(viewquery);
+	result5 = client.query(scorequery);
+	result6 = client.query(datequery);
+	res7 = "{ " + await result3 + " " + await result4 + " " + await result5 + " " + await result6 + " }";
 //	res7 = "{ " + JSON.stringify(result2) + " " + JSON.stringify(result3) + " " + JSON.stringify(result4) + " " + JSON.stringify(result5) + " " + JSON.stringify(result6) + " }";
 	console.log(res7);
 	//res = JSON.parse(res7);
