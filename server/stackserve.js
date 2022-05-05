eb8432b8c895523df5eddefbd54ec674fd1c9990
POL0ROID <donflamon@gmail.com>
2022-05-04 19:12:36 -0500

    commit

--- server/stackserve.js
+++ server/stackserve.js
@@ -9,7 +9,7 @@ const parser = require('koa-bodyparser');
 const serve = require('koa-static');
 const app = new Koa();
 const router = new Router();
-app.use(serve('~/utsa-stacksearch/server/'));
+app.use(serve('.'));
 
 router.post("/stackserve.js", async (ctx, next) => {
 	const client = new Client({
